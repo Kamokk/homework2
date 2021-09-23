31c10c195019b21ae0ea135fa989958b9a141802
C1 <yarievakamila@gmail.com>
2021-09-23 23:46:38 +0300

    add css selector for elements

--- src/main/java/ru/yarieva/Main.java
+++ src/main/java/ru/yarieva/Main.java
@@ -2,6 +2,8 @@ package ru.yarieva;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("text Main");
+
+        System.out.println("Start test");
+
     }
 }
--- src/test/java/ru/yarieva/test/TextBoxTest.java
+++ src/test/java/ru/yarieva/test/TextBoxTest.java
@@ -1,9 +1,12 @@
 package ru.yarieva.test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.SelectRadio;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,13 +14,41 @@ public class TextBoxTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized=true;
+        open("https://demoqa.com/automation-practice-form");
     }
     @Test
     void fillFormTest() {
-open("https://demoqa.com/automation-practice-form");
 $("#firstName").setValue("Mira");
 $("#lastName").setValue("Smith");
 
+//email
+        $("#userEmail").setValue("Mira@mail.ru");
+
+//gender
+   //     $("#gender-radio-2").setValue("Female");
+  $(byText("Female")).click();
+
+
+        // Mobile
+        $("#userNumber").setValue("+79802507861");
+
+        //Date of Birth
+
+        //Subjects
+     //   $("#subjectsContainer").setValue("group");
+
+        // Hobbies
+     $(byText("Music")).click();
+
+        //Picture
+      //  $("#")
+
+        //Current Address
+        $("#currentAddress").setValue("Volgogradsky prospect, 21 st9");
+
+        //Select State
+
+        //Select City
 
     }
 }
